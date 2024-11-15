bfbbd89599e3bfa796f1c6da1d1bf338abf3a98f
tmaog <tommasoguidolin01@gmail.com>
2024-11-15 18:26:45 +0100

    routes update

--- src/routes/openai.routes.ts
+++ src/routes/openai.routes.ts
@@ -5,5 +5,10 @@ const router = express.Router();
 
 router.post("/genRes", OpenAiControllers.genResource);
 router.post("/genGraph", OpenAiControllers.genConceptMap);
+router.post("/MaterialAnalyser", OpenAiControllers.genResource);
+router.post("/LearningObjectiveGenerator", OpenAiControllers.genResource);
+router.post("/MaterialGenerator", OpenAiControllers.genResource);
+router.post("/Summarizer", OpenAiControllers.genResource);
+router.post("/ActivityGenerator", OpenAiControllers.genResource);
 
 export default router;
